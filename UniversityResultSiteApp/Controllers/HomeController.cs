bc019efef7bfa4b1d3e5175c5a52f6c1db32ca9a
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-05-05 17:11:29 +0530

    Add project files.

---
+++ UniversityResultSiteApp/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityResultSiteApp.Models;
+
+namespace UniversityResultSiteApp.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        // GET: Home
+        public ActionResult StudentResult()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult StudentResultView(string testChar)
+        {
+            Student student = new Student();
+
+            string strcon = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(strcon);
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("PersonTopView", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("testChar", testChar);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                student.ID = Convert.ToInt32(reader["ID"]);
+                student.Name = reader["Name"].ToString();
+                student.Malayalam =Convert.ToInt32( reader["Malayalam"]);
+                student.English = Convert.ToInt32(reader["English"]);
+                student.Hindi = Convert.ToInt32(reader["Hindi "]);
+                int total = student.Malayalam + student.Hindi + student.English;
+                if(total>200)
+                {
+                    student.Status = "Pass";
+
+                }
+                else
+                {
+                    student.Status = "Fail";
+                }
+            }
+            con.Close();
+
+            return View("StudentView",student);
+          
+        }
+
+       
+    }
+}
---
+++ UniversityResultSiteApp/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityResultSiteApp.Models
+{
+    public class Student
+    {
+        public  int ID { get; set; }
+        public String RegisterNumber { get; set; }
+        public String Name { get; set; }
+        public int Malayalam { get; set; }
+        public int English { get; set; }
+        public int Hindi { get; set; }x
+        public String Status { get; set; }
+        public String Number { get; set; }
+
+
+    }
+}
